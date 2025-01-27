b81811cdf84b95a3d28ae075f85314c45c2e7e6a
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-27 12:57:02 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,4 +1,4 @@
-chrome.downloads.onDeterminingFilename.addListener(({filename}, suggest) => {
+chrome.downloads.onDeterminingFilename.addListener(({ filename }, suggest) => {
   let p = filename.lastIndexOf(".");
   let format = filename.slice(p).toLowerCase();
   suggest({ filename: filename.slice(0, p) + (format == ".jpeg" || format == ".jfif" ? ".jpg" : format) });
