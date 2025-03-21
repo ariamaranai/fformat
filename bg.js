9de3b6184de7edc0643332afc6614dd6318dae78
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-21 15:28:03 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,5 +1,10 @@
-chrome.downloads.onDeterminingFilename.addListener(({ filename }, suggest) => {
-  let p = filename.lastIndexOf(".");
-  let format = filename.slice(p).toLowerCase();
-  suggest({ filename: filename.slice(0, p) + (format == ".jpeg" || format == ".jfif" ? ".jpg" : format) });
+
+chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+  if (!item.byExtensionId) {
+    let filename = item.filename;
+    let p = filename.lastIndexOf(".");
+    let format = filename.slice(p).toLowerCase();
+    suggest({ filename: filename.slice(0, p) + (format == ".jpeg" || format == ".jfif" ? ".jpg" : format) });
+  }
+  return !0
 });
