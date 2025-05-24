3e2bb7f8acad276590e4760016448cad0bd4b2a1
a <106607480+ariamaranai@users.noreply.github.com>
2025-05-24 23:17:07 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,10 +1,12 @@
 chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
-  item.byExtensionId ||
-  suggest({
-    filename: item.filename.replace(/\.[^.]+$/, e => {
-      let s = e.toLowerCase();
-      return s != ".jpeg" && s != ".jfif" ? s != "mov" && s != ".m4v" ? s : ".mp4" : ".jpg";
+  try {
+    item.byExtensionId ||
+    suggest({
+      filename: item.filename.replace(/\.[^.]+$/, e => {
+        let s = e.toLowerCase();
+        return s != ".jpeg" && s != ".jfif" ? s != "mov" && s != ".m4v" ? s : ".mp4" : ".jpg";
+      })
     })
-  });
+  } catch (e) {}
   return !0
 });
