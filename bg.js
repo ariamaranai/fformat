0f8c51a250f62f85ef0b7130bde8d3bf69e58d0d
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-01 16:29:57 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -4,7 +4,7 @@ chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
     suggest({
       filename: item.filename.replace(/\.[^.]+$/, e => {
         let s = e.toLowerCase();
-        return s != ".jpeg" && s != ".jfif" ? s != ".mov" && s != ".m4v" ? s : ".mp4" : ".jpg";
+        return s != ".jpeg" && s != ".jfif" ? s : ".jpg";
       })
     })
   } catch (e) {}
